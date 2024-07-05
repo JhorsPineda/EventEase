2498b4d9107447ba14a3cf35e166e113c7c97108
jorgeDev <jhorspineda30@gmail.com>
2024-07-05 17:38:30 -0500

    Carpetas Backend y Frontend agregadas al Git

---
+++ Backend/src/test/java/EventEase/EventEase/EventEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package EventEase.EventEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
